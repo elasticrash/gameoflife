b6b72ba4053c36e048c075e14a2f4d347edfe053
stefano <s.kouroupis@gmail.com>
2019-03-05 23:08:51 +0000

    done

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const gridOfLife = [];
-const gridWidth = 30;
-const gridHeight = 30;
+let gridOfLife = [];
+const gridColumns = 30;
+const gridRows = 30;
 
 (function () {
 
-    for (let i = 0; i < gridWidth; i++) {
-        for (let j = 0; j < gridWidth; j++) {
-            gridOfLife.push([i, j]);
+    for (let i = 0; i < gridColumns; i++) {
+        gridOfLife[i] = [];
+        for (let j = 0; j < gridRows; j++) {
+            gridOfLife[i][j] = {
+                position: [i, j],
+                state: getRandomInt(2)
+            };
         }
     }
 
     full();
+
+    setInterval(() => {
+        const canvas = document.getElementById("cnv");
+        nextStep();
+        draw(canvas);
+    }, 1000);
+
     window.addEventListener("resize", () => {
         full();
     });
@@ -26,16 +37,73 @@ function full() {
 
 function draw(canvas) {
     var ctx = canvas.getContext("2d");
-    gridOfLife.forEach(element => {
-        const size = 20;
-        ctx.rect(element[0] * size, element[1] * size, size, size);
-        ctx.stroke();
-        if (Math.round(Math.random()) === 1) {
-            ctx.fillStyle = "#000000";
-            ctx.fillRect(element[0] * size, element[1] * size, size, size);
-        } else {
-            ctx.fillStyle = "#FF00FF";
-            ctx.fillRect(element[0] * size, element[1] * size, size, size);
-        }
+    gridOfLife.forEach((column) => {
+        column.forEach(row => {
+            const element = row.position;
+            const size = 20;
+            ctx.rect(element[0] * size, element[1] * size, size, size);
+            ctx.stroke();
+            if (row.state === 0) {
+                ctx.fillStyle = "#4c1f36";
+                ctx.fillRect(element[0] * size, element[1] * size, size, size);
+            } else {
+                ctx.fillStyle = "#659D32";
+                ctx.fillRect(element[0] * size, element[1] * size, size, size);
+            }
+        });
     });
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getAlive(x, y) {
+    let alive = 0;
+
+    for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+            if (x + i > 0 && y + j > 0 && x + i < gridColumns && y + j < gridRows) {
+                if (gridOfLife[x + i][y + j].state === 1 && i !== 0 && j !== 0) {
+                    alive++;
+                }
+            }
+        }
+    }
+    return alive;
+}
+
+function nextStep() {
+    const nextGridOfLife = [];
+    for (let i = 0; i < gridColumns; i++) {
+        nextGridOfLife[i] = [];
+        for (let j = 0; j < gridRows; j++) {
+            const live = getAlive(i, j);
+            if (gridOfLife[i][j].state === 1) {
+                if (live < 2) {
+                    nextGridOfLife[i][j] = setState(i, j, 0);
+                }
+                if (live === 2 || live == 3) {
+                    nextGridOfLife[i][j] = setState(i, j, 1);
+                }
+                if (live > 3) {
+                    nextGridOfLife[i][j] = setState(i, j, 0);
+                }
+            } else {
+                if (live == 3) {
+                    nextGridOfLife[i][j] = setState(i, j, 1);
+                } else {
+                    nextGridOfLife[i][j] = setState(i, j, 0);
+                }
+            }
+        }
+    }
+    gridOfLife = nextGridOfLife;
+}
+
+function setState(i, j, state) {
+    return {
+        position: [i, j],
+        state: state
+    };
 }
